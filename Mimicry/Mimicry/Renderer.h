9b3c1b661ec056995589327bc4e62d590ba90516
ianco99 <71154684+ianco99@users.noreply.github.com>
2023-08-26 17:44:58 -0300

    Created vertex and fragment shaders in renderer class

--- Mimicry/Mimicry/Renderer.h
+++ Mimicry/Mimicry/Renderer.h
@@ -2,6 +2,27 @@
 class Renderer
 {
 private:
+
+#pragma region FRAGMENT SHADER
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\0";
+    unsigned int fragmentShader;
+#pragma endregion
+
+#pragma region VERTEX SHADER
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+    unsigned int vertexShader;
+#pragma endregion
+
     unsigned int VBO;
 
     float vertices[9] = {
@@ -15,7 +36,11 @@ public:
 	~Renderer();
 
     void InitRenderer();
+    void InitVertexShader();
+    void InitFragmentShader();
 
     void ClearFrame();
 	void RenderFrame();
+
+    void CompileErrorCheck(unsigned int shader);
 };
