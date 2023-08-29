a00d1bf365a553e3feab81edbe2e37db399a2749
ianco99 <iankuz25@gmail.com>
2023-08-29 13:00:25 -0300

    Reorganized project

---
+++ Mimicry/src/BaseGame/BaseGame.cpp
@@ -0,0 +1,24 @@
+#include "BaseGame/BaseGame.h"
+
+BaseGame::BaseGame()
+{
+}
+
+BaseGame::~BaseGame()
+{
+}
+
+void BaseGame::Run()
+{
+
+}
+
+void BaseGame::Init()
+{
+
+}
+
+void BaseGame::Deinit()
+{
+
+}
