e827b8a42092f607fea52abc86b394e9327849cd
OkamiDesu <Tomaspp515@gmail.com>
2023-12-05 14:21:07 -0300

    Delete Mimicry

--- Mimicry/Mimicry/TextureImporter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include "../src/Engine/Exports.h"
-class MimicryEngine_API TextureImporter
-{
-public:
-	static bool LoadTexture(const std::string &path, unsigned int &texture, int &width, int &height, int &channels);
-};
-
